a45f6dab0774c6396e5b41e770cdbf6ac208543f
ahlil23 <39225525+ahlil23@users.noreply.github.com>
2020-02-02 15:56:35 -0600

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,32 @@
+// Select color input
+// Select size input
+let color = document.getElementById('colorPicker');
+let table = document.getElementById('pixelCanvas');
+let sizePicker = document.getElementById('sizePicker');
+let height = document.getElementById('inputHeight').value;
+let width = document.getElementById('inputWidth').value;
+makegrid(height,width);
+
+sizePicker.addEventListener('click', (e) => {
+    e.preventDefault();
+    let height = document.getElementById('inputHeight').value;
+    let width = document.getElementById('inputWidth').value;
+    table.firstElementChild.remove();
+    makegrid(height, width);
+});
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid(height, width) {
+
+  for (let i = 0; i <= height; i++){
+      let row = table.insertRow(i);
+      for (let j = 0; j <= width; j++){
+          let cell = row.insertCell(j);
+          cell.addEventListener('click', (e) => {
+              console.log(e);
+              cell.style.backgroundColor = color.value;
+          });
+      }
+  }
+}
